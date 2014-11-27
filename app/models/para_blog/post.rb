aaffe7b08012a1e7b51402c6ff999fbf00607a41
zenati <zenati@informatique.univ-paris-diderot.fr>
2014-11-27 15:36:58 +0100

    Adds nested comments

--- app/models/para_blog/post.rb
+++ app/models/para_blog/post.rb
@@ -3,6 +3,8 @@ module ParaBlog
     acts_as_taggable
 
     has_many :comments, class_name: 'ParaBlog::Comment', dependent: :destroy
+    accepts_nested_attributes_for :comments, reject_if: :all_blank
+
     belongs_to :component, class_name: 'Para::Component::Base'
   end
 end
